5ae7223841e55147f5f8c4f4c2eaec86be1bdf21
SinHendrix <wahyuardy18official@gmail.com>
2020-12-17 11:50:27 +0700

    done

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php 
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "uploadface";
+
+    $conn = mysqli_connect($host, $username, $password, $database);
+?>
---
+++ log.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+    <?php
+    include('inc.php');
+    ?>
+    </head>
+    <body>
+    <div class="container-fluid">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Username</th>
+                        <th>Timestamp</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        include('connection.php');
+    
+                        $q = $conn->real_escape_string('SELECT * FROM log');
+                        $q = $conn->query($q);
+                        $i = 1;
+                        while($res = $q->fetch_assoc()):
+                    ?>
+    
+                    <tr>
+                        <td class="align-middle"><?= $i++ ?></td>
+                        <td class="align-middle"><?= $res['username'] ?></td>
+                        <td class="align-middle"><?= $res['timestamp'] ?></td>
+                    </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+    </div>
+    </body>
+</html>
